f6e172cb40894fb00cf658b790388c43c9406a63
sen-vip <sen1com2@gmail.com>
2026-09-10 13:11:36 +0900

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "학교회계 예산현황판",
@@ -23,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`${geistSans.variable} antialiased`}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
